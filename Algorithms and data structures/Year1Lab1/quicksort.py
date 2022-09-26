f810867f426993fa544998d62c8ee1d31b64cbdf
NetL01 <razuvaev_nik@list.ru>
2022-09-26 22:57:37 +0300

    quicksort realisation :)

---
+++ Algorithms and data structures/Year1Lab1/quicksort.py
@@ -0,0 +1,12 @@
+n = int(input())
+m = list(map(int, input().split()))
+def quick_sort(s):
+    if len(s) <= 1:
+        return s
+    elem = s[0]
+    left = (list(filter(lambda x: x < elem, s)))
+    center = [i for i in s if i == elem]
+    right = (list(filter(lambda x: x > elem, s)))
+    return quick_sort(left) + center + quick_sort(right)
+
+print(*quick_sort(m))
