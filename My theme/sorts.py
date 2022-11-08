d7e2bb1250ff512ec0aa3c615f7a81f68e3301bf
Nikita <rkraud@bk.ru>
2022-11-08 12:52:45 +0300

    a lot of changes

--- My theme/sorts.py
+++ My theme/sorts.py
@@ -1,38 +1,54 @@
 from sorts_base import Sorts
 import sys
 from random import randint
+import time
+from tqdm import tqdm
+
 
 class Main:
     def __init__(self):
-        print('Start Operations (Main class).')
-        self.sortslib = Sorts()
+        self.sortslib = None
+        self.Sorts_method_list = None
+        print('Start Environment')
         self.dict_func = {}
-        self.Sorts_method_list = [method for method in dir(Sorts) if method.startswith('__') is False]
         self.test_massive = [randint(0, 1000000) for i in range(randint(100, 10000))]
 
+        # trying to connect with included libraries
+        self.connect()
+
+    def connect(self):
         try:
             self.sortslib
+            self.sortslib = Sorts()
+            self.Sorts_method_list = [method for method in dir(Sorts) if method.startswith('__') is False]
+            print('There are:', len(self.Sorts_method_list), 'included methods')
+            for i in tqdm(self.Sorts_method_list):
+                time.sleep(1)
+            print('Connection completed')
         except:
-            print('An error occurred while trying to load the sorts from .../sorts_base.py.')
+            print('Connection failed:')
+            print('An error occurred while trying to load the sorts from .../sorts_base.py')
             print('Check it out in your folder.')
             sys.exit(1)
         self.operations()
 
     def operations(self):
         self.t_clear()
-        print('Waiting for your command:')
+        print('Waiting for your command (sorts):')
         oper = input("$ ")
         if oper == 'help':
             print('Available commands at this step: ')
             print('I don"t want to write documentation, I"m a lazy ass.')
             print('Or push something another for turn back.')
             self.operations()
-        if oper =='exit':
+        if oper == 'exit':
             self.exit(1)
         if oper == 'sorts':
             print('Available commands at this step: ')
-            print('sorts list/ .. or push something another for turn back.')
+            print('<sorts list>/<sorts stress test> .. or push something another for turn back.')
             input_sort = input('$ ')
+
+
             if input_sort == 'sorts list':
                 flag_to_change_test_massive = input('A u want to change test massive? Y/n:')
                 if flag_to_change_test_massive in ['Y', 'y']:
@@ -44,9 +60,20 @@ class Main:
                 if input_sort2 == 'insertion':
                     print(self.sortslib.insertion(self.test_massive))
                     self.operations()
+                if input_sort2 == 'quicksort':
+                    print(self.sortslib.quicksort(self.test_massive))
                 else:
                     print('Turning back...')
                     self.operations()
+
+            if input_sort == 'sorts stress test':
+                flag_to_change_test_massive = input('A u want to change test massive? Y/n:')
+                if flag_to_change_test_massive in ['Y', 'y']:
+                    self.test_massive = list(map(int, input('Enter your test massive: ').split()))
+                    print('Your massive saved.')
+                print('Quicksort test: ', self.sortslib.quicksort(self.test_massive))
+                print('Insertion test: ', self.sortslib.insertion(self.test_massive))
+                self.operations()
 
                 self.operations()
             else:
@@ -56,6 +83,9 @@ class Main:
         if oper not in self.Sorts_method_list:
             print('Oops... Try another command!')
             self.operations()
+
+    # ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
     def t_clear(self):
         print(' ')
--- My theme/sorts_base.py
+++ My theme/sorts_base.py
@@ -1,5 +1,4 @@
- import time
-
+import time
 
 
 class Sorts:
@@ -8,49 +7,34 @@ class Sorts:
         return
 
     def feedback(self):
-        print('Sorts list loaded')
+        print('<Sorts feedback>')
         return
 
+# QUICKSORT ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     def quicksort(self, data):
+        start_time = time.time()
+        self.quicksort_do(data)
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+    def quicksort_do(self, data):
         if len(data) <= 1:
             return data
         border = data[0]
         left = list(filter(lambda x: x < border, data))
         center = [x for x in data if x == border]
         right = list(filter(lambda x: x > border, data))
-        return self.quick_sort(left) + center + self.quick_sort(right)
+        return self.quicksort_do(left) + center + self.quicksort_do(right)
 
     # to do
-    def quicksort_by_step(self, data):
-        if len(data) <= 1:
-            return data
-        border = data[0]
-        left = list(filter(lambda x: x < border, data))
-        center = [x for x in data if x == border]
-        right = list(filter(lambda x: x > border, data))
-        return self.quicksort_by_step(left) + center + self.quicksort_by_step(right)
 
-    # ----------------------------------------------------------
+#INSERTION//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     def insertion(self, data):
         start_time = time.time()
-        print(start_time)
-        for i in range(len(data)):
-            j = i - 1
-            key = data[i]
-            while data[j] > key and j >= 0:
-                data[j + 1] = data[j]
-                j -= 1
-            data[j + 1] = key
-        print(data)
-        a = time.time()
-        print(a)
+        self.insertion_do(data)
         print("--- %s seconds ---" % (time.time() - start_time))
-        return
 
-
-    def insertion_by_step(self, data):
-        step = 1
+    def insertion_do(self, data):
         for i in range(len(data)):
             j = i - 1
             key = data[i]
@@ -58,7 +42,4 @@ class Sorts:
                 data[j + 1] = data[j]
                 j -= 1
             data[j + 1] = key
-            if j != -1 and j != 0:
-                print('Step:', step, ':', data)
-                step += 1
-        return data
+        return
